0822a4cda1b08719fdb344319670f53d9346477c
tunahantyl <70982825+tunahantyl@users.noreply.github.com>
2024-12-09 20:52:18 +0300

    Delete tunanın/ysaodevalintisi-main/yapaySinirAglari directory

--- tunanın/ysaodevalintisi-main/yapaySinirAglari/Form1.cpp
+++
@@ -1,4 +0,0 @@
-#include "pch.h"
-
-// #include "Form1.h"
-
--- tunanın/ysaodevalintisi-main/yapaySinirAglari/Process.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-float* Add_Data(float* sample, int Size, float* x, int Dim);
-float* Add_Labels(float* Labels, int Size, int label);
-float* init_array_random(int len);
-float* init_array_zero(int len);
-void push_back(float*** momentW, int lIndex, int wIndex, int size, float item);
-float* Batch_Norm(float* Samples, int numSample, int inputDim, float mean[], float variance[], bool copy = true);
-int YPoint(int x, float w[], float bias, float Carpan = 1.0);
